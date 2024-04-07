b281f64f37e76908383ab7e9bbfea0fb64e81fc9
Jader <jadermarcelo95@gmail.com>
2024-04-07 01:58:03 -0300

    Initial Commit

---
+++ src/main/java/rocketseat/com/passin/domain/attendee/exceptions/AttendeeAlreadyExistException.java
@@ -0,0 +1,9 @@
+package rocketseat.com.passin.domain.attendee.exceptions;
+
+public class AttendeeAlreadyExistException extends RuntimeException {
+
+    public AttendeeAlreadyExistException(String message) {
+        super(message);
+
+    }
+}
---
+++ src/main/java/rocketseat/com/passin/dto/event/EventResponseDTO.java
@@ -0,0 +1,15 @@
+package rocketseat.com.passin.dto.event;
+
+import lombok.Getter;
+import rocketseat.com.passin.domain.event.Event;
+import rocketseat.com.passin.dto.event.EventDetailDTO;
+
+@Getter
+public class EventResponseDTO {
+
+    EventDetailDTO event;
+
+    public EventResponseDTO(Event event, Integer numberOfAttendees){
+        this.event = new EventDetailDTO(event.getId(), event.getTitle(), event.getDetails(), event.getSlug(), event.getMaximumAttendees(), numberOfAttendees);
+    }
+}
